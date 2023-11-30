9061a928ab17bcc4b8981f8f6a5976935ccc02f1
Ooz47 <yannick.abaul@gmail.com>
2023-11-30 16:56:28 -0400

    feat: contenu non-indexé
    Done

--- web/modules/custom/reseau_mod_up/modules/reseau_mod_up_nonindexe/js/indexation.js
+++ web/modules/custom/reseau_mod_up/modules/reseau_mod_up_nonindexe/js/indexation.js
@@ -7,27 +7,32 @@
       attach: function (context, settings) {
         // console.log("hello");
 
-var test =  $( 'input[name="simple_sitemap[default][index]"]' );
+var inputsSitemap =  $( 'input[name="simple_sitemap[default][index]"]' );
+var inputMenu =  $( 'input[name="menu[enabled]"]' );
+var detailsMenu =  $( 'details[id="edit-menu"]' );
 
-$(context).find(".form-item-field-contenu-indexation").once("form-item-field-contenu-indexation").each(function () {
-    // console.log($(this).find("input").first().attr("name"));
+$(context).find(".field--name-field-contenu-indexation").once("form-item-field-contenu-indexation").each(function () {
+    
 
     var inputName = $(this).find("input").first().attr("name");
     var value = $( 'input[name="'+inputName+'"]:checked' ).val();
-
+    console.log(value);
    //Au chargement de l'entité on vérifie sur un choix est selectionnée
     if(value === undefined) {
-        // si undefined on coche le premier
-        $( 'input[name="'+inputName+'"]' )[0].checked= true;
-        $( 'input[name="'+inputName+'"]' ).val(0);
+        // si undefined on coche le deuxièmre : "oui"
+        $( 'input[name="'+inputName+'"]' )[1].checked= true;
+        // $( 'input[name="'+inputName+'"]' )[1].val(1);
     }
 
     $('input[name="'+inputName+'"]').change(function(){
         var value = $( 'input[name="'+inputName+'"]:checked' ).val();
         // var champFonction = $(this).closest(".field--name-field-ctc-ref-handicap").siblings(".field--name-field-ctc-fonction").find("input");
         
-        // console.log(test);
-
+        // console.log(inputsSitemap);
+        // console.log(this);
+        // console.log($(this));
+        // console.log($(this).val());
+        //Indexation autorisé
         if(value == 1) {
             // console.log(value);
           
@@ -35,16 +40,24 @@ $(context).find(".form-item-field-contenu-indexation").once("form-item-field-con
             // console.log(champFonction.val());
             // if(champFonction.val() === '' ) {
                 // champFonction.val("Référent Handicap");
-                test[0].checked= true;
-                console.log($( '#edit-simple-sitemap-default-index-0' ));
-                // console.log(test);
-                test.val(0);
+                // test[0].checked= true;
+                console.log(inputMenu);
+                console.log(detailsMenu);
+                inputsSitemap[1].checked= true;
+                detailsMenu.show(100);
+                // inputMenu[0].checked= true;
+                // // console.log($( '#edit-simple-sitemap-default-index-0' ));
+                // // console.log(test);
+                // test.val(0);
             // }
         }
         else {
-       
-            test[1].checked= true;
-            test.val(1);
+            //Indexation non autorisé
+            inputMenu[0].checked= false;
+            // inputMenu[0].val(0);
+            detailsMenu.hide(100);
+            inputsSitemap[0].checked= true;
+            // test.val(1);
         }
 
         });
