3b1979ef6d828f909d4da95dc4bb30d1f073c3a5
Ooz47 <yannick.abaul@gmail.com>
2023-12-28 21:20:42 -0400

    Fix: Correctifs dev fonctionnalités indépendantes
    non-indexé: bug affiche paramètre menu alors que sur non indexé + accès au paramètres simple xml sitemap pour webmaster, pageprivee: template se charge pour toutes les pages de base, révisions + blog: retrait de case à cocher quand nouveau contenu

--- web/modules/custom/reseau_mod_up/modules/reseau_mod_up_nonindexe/js/indexation.js
+++ web/modules/custom/reseau_mod_up/modules/reseau_mod_up_nonindexe/js/indexation.js
@@ -1,72 +1,60 @@
 (function ($, Drupal, window, document) {
 
-    'use strict';
-  
-    // To understand behaviors, see https://drupal.org/node/756722#behaviors
-    Drupal.behaviors.indexation = {
-      attach: function (context, settings) {
-        // console.log("hello");
+  'use strict';
 
-var inputsSitemap =  $( 'input[name="simple_sitemap[default][index]"]' );
-var inputMenu =  $( 'input[name="menu[enabled]"]' );
-var detailsMenu =  $( 'details[id="edit-menu"]' );
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.indexation = {
+    attach: function (context, settings) {
+      // console.log("hello");
 
-$(context).find(".field--name-field-contenu-indexation").once("form-item-field-contenu-indexation").each(function () {
-    
+      var inputsSitemap = $('input[name="simple_sitemap[default][index]"]');
+      var inputMenu = $('input[name="menu[enabled]"]');
+      var detailsMenu = $('details[id="edit-menu"]');
 
-    var inputName = $(this).find("input").first().attr("name");
-    var value = $( 'input[name="'+inputName+'"]:checked' ).val();
-    console.log(value);
-   //Au chargement de l'entité on vérifie sur un choix est selectionnée
-    if(value === undefined) {
-        // si undefined on coche le deuxièmre : "oui"
-        $( 'input[name="'+inputName+'"]' )[1].checked= true;
-        // $( 'input[name="'+inputName+'"]' )[1].val(1);
-    }
+      $(context).find(".field--name-field-contenu-indexation").once("form-item-field-contenu-indexation").each(function () {
 
-    $('input[name="'+inputName+'"]').change(function(){
-        var value = $( 'input[name="'+inputName+'"]:checked' ).val();
-        // var champFonction = $(this).closest(".field--name-field-ctc-ref-handicap").siblings(".field--name-field-ctc-fonction").find("input");
-        
-        // console.log(inputsSitemap);
-        // console.log(this);
-        // console.log($(this));
-        // console.log($(this).val());
-        //Indexation autorisé
-        if(value == 1) {
+
+        var inputName = $(this).find("input").first().attr("name");
+        var value = $('input[name="' + inputName + '"]:checked').val();
+        console.log(value);
+        //Au chargement de l'entité on vérifie sur un choix est selectionnée
+        if (value === undefined) {
+          // si undefined on coche le deuxièmre : "oui"
+          $('input[name="' + inputName + '"]')[1].checked = true;
+          // $( 'input[name="'+inputName+'"]' )[1].val(1);
+        }
+
+        if (value == false) {
+          // si est sur non, on cache option du menu
+          detailsMenu.hide(1);
+        }
+
+        $('input[name="' + inputName + '"]').change(function () {
+          var value = $('input[name="' + inputName + '"]:checked').val();
+
+          //Indexation autorisé
+          if (value == 1) {
             // console.log(value);
-          
-            // console.log(champFonction);
-            // console.log(champFonction.val());
-            // if(champFonction.val() === '' ) {
-                // champFonction.val("Référent Handicap");
-                // test[0].checked= true;
-                console.log(inputMenu);
-                console.log(detailsMenu);
-                inputsSitemap[1].checked= true;
-                detailsMenu.show(100);
-                // inputMenu[0].checked= true;
-                // // console.log($( '#edit-simple-sitemap-default-index-0' ));
-                // // console.log(test);
-                // test.val(0);
+            // console.log(inputMenu);
+            // console.log(detailsMenu);
+            inputsSitemap[1].checked = true;
+            detailsMenu.show(100);
+
             // }
-        }
-        else {
+          } else {
             //Indexation non autorisé
-            inputMenu[0].checked= false;
-            // inputMenu[0].val(0);
+            inputMenu[0].checked = false;
             detailsMenu.hide(100);
-            inputsSitemap[0].checked= true;
-            // test.val(1);
-        }
+            inputsSitemap[0].checked = true;
+          }
 
         });
 
-// console.log(this);
-  });
+        // console.log(this);
+      });
 
 
     }
-};
+  };
 
 })(jQuery, Drupal, this, this.document);
